4cab774ffe344fcbe473c688efaa4cf88cb69562
Shriyagautam12 <gautamshriya5@gmail.com>
2023-05-12 20:25:44 +0530

    Second Commit
    Co-authored-by: Tashi Gautam <tashi2408@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import './App.css';
+import Register from './Register';
+import Login from './Login';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-         Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Login />
+    </main>
   );
 }
 
