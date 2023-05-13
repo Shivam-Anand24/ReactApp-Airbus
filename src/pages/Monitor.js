32514cfad01e8179b4f8f836385403cdaac7f86f
Shriyagautam12 <gautamshriya5@gmail.com>
2023-05-13 14:52:28 +0530

    okay completed

--- src/pages/Monitor.js
+++ src/pages/Monitor.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-export const Monitor = () => {
-  return <div>Monitor</div>;
+ const Monitor = () => {
+  return <h1>Hello  Officer User Here You Can see The Details Of Your Machinery Department</h1>;
 };
+
+export default Monitor;
