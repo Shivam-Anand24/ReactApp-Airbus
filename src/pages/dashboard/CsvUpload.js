9f5a67c8940706274a867b3110ac9a7246d0e3c7
tashi2408 <63187583+tashi2408@users.noreply.github.com>
2023-05-13 15:21:34 +0530

    csv upload

--- src/pages/dashboard/CsvUpload.js
+++ src/pages/dashboard/CsvUpload.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import axios from "../../api/axios";
-import CloudUploadOutlinedIcon from '@material-ui/icons/CloudUploadOutlined';
+import CloudUploadOutlinedIcon from "@material-ui/icons/CloudUploadOutlined";
 import Typography from "@mui/material/Typography";
-
+import Snackbar from "@mui/material/Snackbar";
 
 const InvisibleInput = styled("input")({
   display: "none",
@@ -12,14 +12,16 @@ const InvisibleInput = styled("input")({
 
 function CSVUploader() {
   const [file, setFile] = useState(null);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
-  const handleFileUpload = async (event) => {
-    console.log("here");
-    const selectedFile = event.target.files[0];
+  const handleFileUpload = async () => {
+    const selectedFile = file;
+    console.log(file, "here");
     const formData = new FormData();
     formData.append("csv_file", selectedFile);
     formData.append("key", "fabrication");
@@ -29,51 +31,71 @@ function CSVUploader() {
           "Content-Type": "multipart/form-data",
         },
       });
-      console.log(response.data);
+      setSnackbarMessage(response.data);
     } catch (error) {
-      console.error(error);
+      setSnackbarMessage(error);
     }
     setFile(selectedFile);
   };
 
   return (
     <>
-    
-    <Typography variant="body1" sx={{ marginLeft: '8px', fontWeight: 'bold', fontSize: '1.2rem', color: '#333',  fontFamily: 'Poppins' }}>
-    Kindly Upload Your CSV here:-
-  </Typography>
-  <div style={{ display: 'flex', alignItems: 'center', marginLeft: '15px' }}>
-  <label htmlFor="csv-upload">
-  <Typography
-    component="span"
-    variant="body1"
-    sx={{ display: 'inline-flex', alignItems: 'center', cursor: 'pointer',
-     marginLeft: '15px',
-     textDecoration: 'underline', 
-     marginRight: '20px'}}
-  >
-    <CloudUploadOutlinedIcon sx={{ marginRight: '5px' }} />
-    Choose Your File
-  </Typography>
-</label>
-
-
-
-      <InvisibleInput
-        id="csv-upload"
-        type="file"
-        onChange={handleFileChange}
-        accept=".csv"
+      <Snackbar
+        open={snackbarOpen}
+        message={snackbarMessage}
+        autoHideDuration={4000}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        onClose={() => setSnackbarOpen(false)}
       />
-        <Button onClick={handleFileUpload} disabled={!file}
-        variant="contained"
-        style={{ backgroundColor: "#40362A", color: "#ffffff" }}
+      <Typography
+        variant="body1"
+        sx={{
+          marginLeft: "8px",
+          fontWeight: "bold",
+          fontSize: "1.2rem",
+          color: "#333",
+          fontFamily: "Poppins",
+        }}
+      >
+        Kindly Upload Your CSV here:-
+      </Typography>
+      <div
+        style={{ display: "flex", alignItems: "center", marginLeft: "15px" }}
+      >
+        <label htmlFor="csv-upload">
+          <Typography
+            component="span"
+            variant="body1"
+            sx={{
+              display: "inline-flex",
+              alignItems: "center",
+              cursor: "pointer",
+              marginLeft: "15px",
+              textDecoration: "underline",
+              marginRight: "20px",
+            }}
+          >
+            <CloudUploadOutlinedIcon sx={{ marginRight: "5px" }} />
+            Choose Your File
+          </Typography>
+        </label>
 
-        startIcon={<CloudUploadOutlinedIcon />}
+        <InvisibleInput
+          id="csv-upload"
+          type="file"
+          onChange={handleFileChange}
+          accept=".csv"
+        />
+        <Button
+          onClick={() => handleFileUpload()}
+          disabled={!file}
+          variant="contained"
+          style={{ backgroundColor: "#40362A", color: "#ffffff" }}
+          startIcon={<CloudUploadOutlinedIcon />}
         >
           Upload
         </Button>
-        </div>
+      </div>
     </>
   );
 }
